141051e561a3c33ed73aa99badc30ab8dc2ffc9e
mchome <280604399@qq.com>
2019-04-08 13:03:00 +0800

    fork camera as local package

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # boomerang poc
 
-[x] init project
-[ ] take image stream
+- [x] init project
+- [x] grab image frames
+- [x] convert [yuv_420_888](https://developer.android.com/reference/android/graphics/ImageFormat.html#YUV_420_888) to jpeg in Android
+- [ ] fix the rotation
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
@@ -15,14 +17,47 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  CameraController _camera;
+  bool _enableCameraPreview = false;
+  Uint8List _image;
+
+  void initState() {
+    super.initState();
+    availableCameras().then((List<CameraDescription> cameras) {
+      _camera = CameraController(cameras.firstWhere((CameraDescription camera) {
+        return camera.lensDirection == CameraLensDirection.front;
+      }), ResolutionPreset.low)
+        ..initialize();
+    });
+  }
+
+  void dispose() {
+    super.dispose();
+    _camera?.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Boomerang poc')),
-      body: Center(),
+      body: Center(
+        child: _camera != null && _enableCameraPreview
+            // ? AspectRatio(
+            //     aspectRatio: _camera.value.aspectRatio,
+            //     child: CameraPreview(_camera),
+            //   )
+            ? Image.memory(_image, gaplessPlayback: true)
+            : Container(height: 200.0, width: 200.0, color: Colors.amber),
+      ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Go',
+        onPressed: () {
+          setState(() => _enableCameraPreview = !_enableCameraPreview);
+          _enableCameraPreview
+              ? _camera.startJpegImageStream((Uint8List image) async {
+                  setState(() => this._image = image);
+                })
+              : _camera.stopImageStream();
+        },
         child: Icon(Icons.camera),
       ),
     );
